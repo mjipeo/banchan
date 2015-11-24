61f8d32cd1d82a55f17034ee212c329fc30a6f71
Minjong Chung <mj@tridge.com>
2015-11-24 17:32:43 +0900

    Add functional prototype

---
+++ banchan/functional.py
@@ -0,0 +1,7 @@
+# You can't trivially replace this with `functools.partial` because this binds
+# to classes and returns bound instances, whereas functools.partial (on
+# CPython) is a type and its instances don't bind.
+def curry(_curried_func, *args, **kwargs):
+    def _curried(*moreargs, **morekwargs):
+        return _curried_func(*(args + moreargs), **dict(kwargs, **morekwargs))
+    return _curried
